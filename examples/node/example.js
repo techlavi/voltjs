3776871213e6444846a6476442ea5b3f13c2be7c
Umakant Patil <>
2021-05-29 00:08:30 +0530

    Add examples

---
+++ examples/node/example.js
@@ -0,0 +1,15 @@
+/**
+ * Example show Volt Js can be used in Node.js
+ */
+
+const fs = require('fs')
+const path = require('path')
+const Volt = require('voltjs/node')
+
+Volt.getTemplate = function (name) {
+  return fs.readFileSync(path.normalize(`${__dirname}/${name}`), {encoding: 'utf-8'})
+}
+
+const voltJs = new Volt()
+
+console.log(voltJs.fetch('demo.volt', {name: "Umakant Patil"}))
